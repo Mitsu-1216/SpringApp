6696fccb5476609ba5db66d45ebcaf3bf8c7eada
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-04-15 09:07:20 +0900

    No.6

--- src/main/java/com/example/demo2/dto/UserRequest.java
+++ src/main/java/com/example/demo2/dto/UserRequest.java
@@ -2,10 +2,15 @@ package com.example.demo2.dto;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 
 /**
  * ユーザー情報 リクエストデータ
+ * Lombokの@Dataを使用してgetterとsetterを書かないようにシンプルな作り
  */
 @Data
 public class UserRequest implements Serializable {
@@ -13,6 +18,8 @@ public class UserRequest implements Serializable {
     /**
      * 名前
      */
+    @NotEmpty(message = "名前を入力してください")
+    @Size(max = 100, message = "名前は100桁以内で入力してください")
     private String name;
 
     /**
