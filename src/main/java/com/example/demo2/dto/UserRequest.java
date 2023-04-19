250e99f1996949def3f5ac28072a99f6909b3771
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-04-19 21:55:33 +0900

    No.8

--- src/main/java/com/example/demo2/dto/UserRequest.java
+++ src/main/java/com/example/demo2/dto/UserRequest.java
@@ -3,6 +3,7 @@ package com.example.demo2.dto;
 import java.io.Serializable;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import lombok.Data;
@@ -24,10 +25,12 @@ public class UserRequest implements Serializable {
     /**
      * 住所
      */
+    @Size(max = 255, message = "住所は255桁以内で入力してください")
     private String address;
 
     /**
      * 電話番号
      */
+    @Pattern(regexp = "0\\d{1,4}-\\d{1,4}-\\d{4}", message = "電話番号の形式で入力してください")
     private String phone;
 }
